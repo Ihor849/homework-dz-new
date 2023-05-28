d8127c2bf3bf7db5f6a9c4f9df0d9f3ab8ff844e
Ihor Marchenko <109768648+Ihor849@users.noreply.github.com>
2023-05-28 13:33:15 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GlobalStyle } from './style/GlobalStyle';
-import { BrowserRouter } from "react-router-dom";
-import {App} from 'components/App/App';
+
+import  App  from 'components/App/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="homework-dz-new">
-      <App />
-    </BrowserRouter >
+    <App>GGGGGGGG</App>
     <GlobalStyle />
   </React.StrictMode>
 );
